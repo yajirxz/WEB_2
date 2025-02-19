72dedebd86306d0b848db8767580ada7aa547a47
Ing Yahir Turing Automata Boolean <yahirrodriguez647@aragon.unam.mx>
2025-02-18 19:46:13 -0600

    and

--- 17022025/app6.js
+++ 17022025/app6.js
@@ -5,3 +5,8 @@ let resultado2 = false  || true;
 console.log(resultado2);
 
 //programar las compuertas and, or y not en consola
+
+let resultadoAnd = false && false;
+let resultadoAnd2 = false && true;
+let resultadoAnd3 = true && false;
+let resultadoAnd4 = true && true;
