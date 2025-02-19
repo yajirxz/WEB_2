89e658199f152b8b4cf5f935be9c2e13d6439be7
Ing Yahir Turing Automata Boolean <yahirrodriguez647@aragon.unam.mx>
2025-02-18 20:20:38 -0600

    Tarea Operadores logicos

---
+++ Tarea_OperadoresLogicos/app.js
@@ -0,0 +1,29 @@
+//Operadores Lógicos
+
+//Compuerta And 
+console.log("Operadores Lógicos: ");
+console.log("   1. Compuerta AND");
+let And1 = false && false;
+let And2 = false && true;
+let And3 = true && false;
+let And4 = true && true;
+console.log("       0 && 0 = "+ And1);
+console.log("       0 && 1 = "+ And2);
+console.log("       1 && 0 = "+ And3);
+console.log("       1 && 1 = "+ And4);
+//Compuerta OR
+console.log("   2. Compuerta OR");
+let Or1 = false || false;
+let Or2 = false || true;
+let Or3 = true  || false;
+let Or4 = true  || true;
+console.log("       0 || 0 = "+ Or1);
+console.log("       0 || 1 = "+ Or2);
+console.log("       1 || 0 = "+ Or3);
+console.log("       1 || 1 = "+ Or4);
+//Compuerta NOT
+console.log("   3. Compuerta NOT");
+let Not1 = !true;
+let Not2 = !false;
+console.log("       True  = "+ Not1);
+console.log("       False = "+ Not2);
